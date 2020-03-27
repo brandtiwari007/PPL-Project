a90082edc3c73496e2804933738ceebdd3a1ada4
raghvendra <raghvendra.tiwari@daffodilsw.com>
2020-03-27 13:11:39 +0530

    ppl_in_hooks

--- frontend/src/components/addCategory.js
+++ frontend/src/components/addCategory.js
@@ -1,60 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-class AddCategory extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      image: ""
-    };
-  }
-  handleSubmit = event => {
+// class AddCategory extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: "",
+//       image: ""
+//     };
+//   }
+
+const AddCategory = props => {
+  const [addcat, setaddcat] = useState({
+    name: "",
+    image: ""
+  });
+
+  const handleSubmit = event => {
     event.preventDefault();
     const formdata = new FormData(event.target);
     axios
       .post("http://localhost:8969/addcats", formdata)
       .then(result => {
-        // alert("succesfully added category");
-        this.props.changeCatState();
-        this.props.getCategory();
+        alert("succesfully added category");
+        props.changeCatState();
+        props.getCategory();
       })
       .catch(err => {
         console.log(err);
       });
   };
-  handlechange = event => {
-    const name = event.target.name;
-    const value = event.target.value;
-    this.setState({
-      [name]: value
+  const handlechange = event => {
+    // const name = event.target.name;
+    // const value = event.target.value;
+    // this.setState({
+    //   [name]: value
+    // });
+    setaddcat({
+      ...addcat,
+      name: event.target.value
     });
   };
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <ul>
-            <li>
-              <label>
-                categories
-                <input type="text" onChange={this.handlechange} name="name" />
-              </label>
-            </li>
-            <br />
-            <li>
-              <label>
-                addImage
-                <input type="file" onChange={this.handlechange} name="image" />
-              </label>
-            </li>
-            <br />
-            <input type="submit" value="submit" />
-          </ul>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <ul>
+          <li>
+            <label>
+              categories
+              <input type="text" onChange={handlechange} name="name" />
+            </label>
+          </li>
+          <br />
+          <li>
+            <label>
+              addImage
+              <input type="file" name="image" />
+            </label>
+          </li>
+          <br />
+          <input type="submit" value="submit" />
+        </ul>
+      </form>
+    </div>
+  );
+};
+//ReactDOM.render(<AddCategory y={5} />, document.getElementById("root"));
 export default AddCategory;
--- frontend/src/components/uploadpost.js
+++ frontend/src/components/uploadpost.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 //import AddCategory from 'addCategory';
-class Upload extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      image: "",
-      title: "",
-      category: "cat",
-      picsrc: "",
-      cat: []
-      // data:''
-    };
-  }
-  handlesubmit = event => {
+// class Upload extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       image: "",
+//       title: "",
+//       category: "cat",
+//       picsrc: "",
+//       cat: []
+//       // data:''
+//     };
+//   }
+
+const Upload = props => {
+  const [up, setUpl] = useState({
+    image: "",
+    title: "",
+    category: "cat",
+    picsrc: ""
+  });
+  const [cat, setCat] = useState([]);
+
+  const handlesubmit = event => {
     event.preventDefault();
     const formdata = new FormData(event.target);
     // formdata.append('image', this.state.image);
@@ -27,7 +37,7 @@ class Upload extends React.Component {
     axios.post("http://localhost:8969/upload", formdata).then(
       result => {
         console.log("submit data", result);
-        this.props.changeCount();
+        props.changeCount();
       }
       // <li>this.state.picsrc</li>
 
@@ -40,72 +50,73 @@ class Upload extends React.Component {
       // <img className="image-work" src="/home/com15/pplproject/public/upload/3c2fc2aa1038d88bdf0ddb152cf86052"  alt="" />
     );
   };
-  handleimage = event => {
-    this.setState({
-      image: event.target.files[0]
+  const handleimage = event => {
+    // this.setState({
+    //   image: event.target.files[0]
+    // });
+    setUpl({
+      image: event.target.value
     });
     console.log("imagr path : ", event.target.files);
   };
-  handlecategory = event => {
-    this.setState({
-      category: event.target.value
-    });
+  const handlecategory = event => {
+    // this.setState({
+    //   category: event.target.value
+    // });
+    setUpl({ ...up, category: event.target.value });
   };
 
-  handletitle = event => {
-    this.setState({
+  const handletitle = event => {
+    // this.setState({
+    //   title: event.target.value
+    // });
+    setUpl({
       title: event.target.value
     });
   };
-  componentDidMount() {
+  // componentDidMount() {
+  useEffect(() => {
     console.log("asdasdadads>>>>>>>>>>> in did mount");
     axios.get("http://localhost:8969/getcats").then(response => {
       if (response.data) {
-        this.setState(
-          {
-            cat: response.data
-          },
-          () => console.log("component did mount", this.state.cat)
-        );
+        // this.setState(
+        //   {
+        //     cat: response.data
+        //   },
+        //   () => console.log("component did mount", this.state.cat)
+        // );
+        setCat(response.data);
       }
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handlesubmit} id="form2">
-          <ul>
-            <li>
-              <span>Select image</span>
-              <input type="file" name="image" onChange={this.handleimage} />
-            </li>
-            <li>
-              <span>Title</span>
-              <input
-                type="text"
-                name="title"
-                onChange={this.handletitle}
-                required
-              />
-            </li>
-            <li>
-              <span>Category</span>
-              <select name="category" onChange={this.handlecategory}>
-                {this.state.cat.map((data, index) => {
-                    return(
-                        <option value={data.name}> {data.name} </option>
-                    )
-                })}
-              </select>
-            </li>
-            <input type="submit" />
-          </ul>
-        </form>
-        {/* {console.log('abc',this.state.data)} */}
-        {/* <img src={this.state.picsrc+this.state.data.image} alt='backend image' /> */}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <form onSubmit={handlesubmit} id="form2">
+        <ul>
+          <li>
+            <span>Select image</span>
+            <input type="file" name="image" onChange={handleimage} />
+          </li>
+          <li>
+            <span>Title</span>
+            <input type="text" name="title" onChange={handletitle} required />
+          </li>
+          <li>
+            <span>Category</span>
+            <select name="category" onChange={handlecategory}>
+              {cat.map((data, index) => {
+                return <option value={data.name}> {data.name} </option>;
+              })}
+            </select>
+          </li>
+          <input type="submit" />
+        </ul>
+      </form>
+      {/* {console.log('abc',this.state.data)} */}
+      {/* <img src={this.state.picsrc+this.state.data.image} alt='backend image' /> */}
+    </div>
+  );
+};
+
 export default Upload;
